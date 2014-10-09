1439d8dae9ef52a7cfd3b1618272214f48e8fd09
Frederic Duby <fcd1@columbia.edu>
2014-10-09 16:20:36 -0400

    Update code for CUL Top Navigation Bar Widget
    Copied and pasted the "Advanced Usage" code given for the
    Top Navigation Bar Widget from the following web page:
    http://cdn.cul.columbia.edu/ldpd-toolkit/widgets/

--- common/footer.php
+++ common/footer.php
@@ -10,19 +10,20 @@
     <p class="copyright-footer"> 
       <?php echo cul_copyright_information();?>
     </p>
+
     <!-- CULTNBW START -->
     <script type="text/javascript">
-    <!-- 
- 	CULh_style = 'staticlink'; // staticlink, static, search, standard (default)
-CULh_width = ''; // fixed, fluid (default)
-//CULh_colorfg = '#333333'; // topnavbar foreground color. hex value. ex: #002B7F
-//CULh_colorbg = '#666666'; // topnavbar background color. hex value. ex: #779BC3
-//CULh_nobs = 1; // uncomment to NOT load our bootstrap javascript file and or use your own (v2.3.x required)
-//CULh_notk = 1; // uncomment to NOT load our ldpd-toolkit.js and or use your own.
-//-->
-    </script>
-    <script src="//ldpd.cul.columbia.edu/ldpd-toolkit/build/js/jquery-1.7.2.min.js"></script>
-    <script src="//ldpd.cul.columbia.edu/ldpd-toolkit/widgets/cultnbw.min.js"></script>
+	CULh_style = 'staticlink'; // limited, staticlink, static, search, standard (default)
+	//CULh_width = ''; // fixed, fluid (default)
+	//CULh_colorfg = '#333333'; // topnavbar foreground color. hex value. ex: #002B7F
+	//CULh_colorbg = '#666666'; // topnavbar background color. hex value. ex: #779BC3
+	//CULh_nobs = 1; // uncomment to NOT load our bootstrap javascript file and or use your own (v2.3.x required)
+	//CULh_notk = 1; // uncomment to NOT load our ldpd-toolkit.js and or use your own.
+	//CULh_links = { "http://clio.columbia.edu/" : "CLIO", "http://hours.library.columbia.edu" : "Hours" }; // custom (CUL/IS menu) dropdown links. "URL1":"TEXT1", "URL2":"TEXT2", "URL3":"TEXT3"
+	//CULh_brand = { url : 'http://library.columbia.edu', tablet : 'Columbia University Libraries', phone : 'Libraries' }; // custom "brand" titles for tablet and phone
+	</script>
+    <script src="//cdn.cul.columbia.edu/ldpd-toolkit/build/js/jquery-cul.js"></script>
+    <script src="//cdn.cul.columbia.edu/ldpd-toolkit/widgets/cultnbw.min.js"></script>
     <!-- /CULTNBW END -->
 
   </body>
