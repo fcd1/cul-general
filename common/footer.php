7dc6f4846201ea9657c756442b7013f061e8f3c5
Frederic Duby <fcd1@columbia.edu>
2014-10-09 16:50:53 -0400

    Add conditional code to handle IE 8 quirkiness
    Added conditional top-padding to body -- for some reason, the CUL
    widget is not able to do it.
    Also do not load jquery-cul.js if IE 8, because the top navigation
    widget gets all messed up.

--- common/footer.php
+++ common/footer.php
@@ -11,7 +11,15 @@
       <?php echo cul_copyright_information();?>
     </p>
 
+
     <!-- CULTNBW START -->
+
+    <!-- fcd1, 09Oct14: Added following conditional due to IE quirk, body not getting -->
+    <!-- padding set by CUL widget, so set it by hand -->
+    <!--[if lte IE 8]>
+    <style> body.hascultnbw {padding-top:50px} </style>
+    <![endif]-->
+
     <script type="text/javascript">
 	CULh_style = 'staticlink'; // limited, staticlink, static, search, standard (default)
 	//CULh_width = ''; // fixed, fluid (default)
@@ -21,8 +29,10 @@
 	//CULh_notk = 1; // uncomment to NOT load our ldpd-toolkit.js and or use your own.
 	//CULh_links = { "http://clio.columbia.edu/" : "CLIO", "http://hours.library.columbia.edu" : "Hours" }; // custom (CUL/IS menu) dropdown links. "URL1":"TEXT1", "URL2":"TEXT2", "URL3":"TEXT3"
 	//CULh_brand = { url : 'http://library.columbia.edu', tablet : 'Columbia University Libraries', phone : 'Libraries' }; // custom "brand" titles for tablet and phone
-	</script>
+    </script>
+    <!--[if ! IE 8]>
     <script src="//cdn.cul.columbia.edu/ldpd-toolkit/build/js/jquery-cul.js"></script>
+    <![endif]-->	
     <script src="//cdn.cul.columbia.edu/ldpd-toolkit/widgets/cultnbw.min.js"></script>
     <!-- /CULTNBW END -->
 
